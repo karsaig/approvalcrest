bc23980e345e7e4e5c9f2683a00e1eaefeebf5c7
karsaig <sonic484@gmail.com>
2020-04-28 00:39:40 +0200

    Renamed release pom

--- release.sh
+++ release.sh
@@ -7,15 +7,15 @@ version=$1
 mvn versions:set -DnewVersion="${version}"
 mvn versions:commit
 
-mvn -f release-pom.xml versions:set -DnewVersion="${version}"
-mvn -f release-pom.xml versions:commit
+mvn -f for-release-pom.xml versions:set -DnewVersion="${version}"
+mvn -f for-release-pom.xml versions:commit
 
 git commit -a -m "Release version ${version}"
 
 mvn clean install
 mvn clean
 
-mvn -f release-pom.xml clean deploy -P sign-release,ossrh --settings ../../Installed/settings.xml
+mvn -f for-release-pom.xml clean deploy -P sign-release,ossrh --settings ../../Installed/settings.xml
 
 git clean -f
 git push
