59d1b97ad94765bee44893c214093ecf96699253
karsaig <sonic484@gmail.com>
2020-04-27 23:27:24 +0200

    Update release script

--- release.sh
+++ release.sh
@@ -6,8 +6,12 @@ version=$1
 
 mvn versions:set -DnewVersion="${version}"
 mvn versions:commit
+
+mvn -f release-pom.xml versions:set -DnewVersion="${version}"
+mvn -f release-pom.xml versions:commit
+
 git commit -a -m "Next release version"
 
 mvn clean install
 
-mvn clean deploy -P sign-release,ossrh --settings ../../Installed/settings.xml
+mvn -f release-pom.xml clean deploy -P sign-release,ossrh --settings ../../Installed/settings.xml
