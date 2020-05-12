c9b490c780c9e84ecfe7c4fc6a4b1d499b0d035c
karsaig <sonic484@gmail.com>
2020-05-12 12:36:46 +0200

    Add next version script

---
+++ nextversion.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -exuo pipefail
+
+version=$1
+
+mvn versions:set -DnewVersion="${version}"
+mvn versions:commit
+
+mvn -f for-release-pom.xml versions:set -DnewVersion="${version}"
+mvn -f for-release-pom.xml versions:commit
+
+git commit -a -m "Next version ${version}"
