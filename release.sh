c1e3a4099f9b749fb5b21c5507e696e937d82ce0
karsaig <sonic484@gmail.com>
2022-05-08 14:53:44 +0200

    Add matrix java build

--- release.sh
+++ release.sh
@@ -1,24 +1,25 @@
 #!/usr/bin/env bash
 
+java.version.to.run=${java.version.to.run}
 set -exuo pipefail
 
 version=$1
 
-mvn versions:set -DnewVersion="${version}"
-mvn versions:commit
+mvn versions:set --global-toolchains .github/ubuntu-toolchains.xml -DnewVersion="${version}"
+mvn versions:commit --global-toolchains .github/ubuntu-toolchains.xml
 
-mvn -f for-release-pom.xml versions:set -DnewVersion="${version}"
-mvn -f for-release-pom.xml versions:commit
+mvn -f for-release-pom.xml versions:set --global-toolchains .github/ubuntu-toolchains.xml -DnewVersion="${version}"
+mvn -f for-release-pom.xml versions:commit --global-toolchains .github/ubuntu-toolchains.xml
 
 git commit -a -m "Release version ${version}"
 
-mvn clean install
-mvn clean
+mvn clean install --global-toolchains .github/ubuntu-toolchains.xml -Djava.version.to.run="${java.version.to.run}"
+mvn clean --global-toolchains .github/ubuntu-toolchains.xml -Djava.version.to.run="${java.version.to.run}"
 
 
-mvn -f for-release-pom.xml clean deploy -P sign-release,ossrh --settings ../../Installed/settings.xml -DskipRemoteStaging=true
+mvn -f for-release-pom.xml clean deploy -P sign-release,ossrh --settings ../../Installed/settings.xml --global-toolchains .github/ubuntu-toolchains.xml -DskipRemoteStaging=true -Djava.version.to.run="${java.version.to.run}"
 
-mvn -f for-release-pom.xml nexus-staging:deploy-staged -P ossrh --settings ../../Installed/settings.xml -DstagingDescription="Description of the staged repository"
+mvn -f for-release-pom.xml nexus-staging:deploy-staged -P ossrh --settings ../../Installed/settings.xml --global-toolchains .github/ubuntu-toolchains.xml -DstagingDescription="Description of the staged repository" -Djava.version.to.run="${java.version.to.run}"
 
 git clean -f
 git push
