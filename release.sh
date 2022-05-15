9e3147303337e704e2a142f8022870a3e7497517
karsaig <sonic484@gmail.com>
2022-05-15 13:16:55 +0200

    Using JDK 17 only

--- release.sh
+++ release.sh
@@ -5,21 +5,21 @@ set -exuo pipefail
 
 version=$1
 
-mvn versions:set --global-toolchains .github/ubuntu-toolchains.xml -DnewVersion="${version}"
-mvn versions:commit --global-toolchains .github/ubuntu-toolchains.xml
+mvn versions:set -DnewVersion="${version}"
+mvn versions:commit
 
-mvn -f for-release-pom.xml versions:set --global-toolchains .github/ubuntu-toolchains.xml -DnewVersion="${version}"
-mvn -f for-release-pom.xml versions:commit --global-toolchains .github/ubuntu-toolchains.xml
+mvn -f for-release-pom.xml versions:set -DnewVersion="${version}"
+mvn -f for-release-pom.xml versions:commit
 
 git commit -a -m "Release version ${version}"
 
-mvn clean install --global-toolchains .github/ubuntu-toolchains.xml -Djava.version.to.run="${java.version.to.run}"
-mvn clean --global-toolchains .github/ubuntu-toolchains.xml -Djava.version.to.run="${java.version.to.run}"
+mvn clean install -Djava.version.to.run="${java.version.to.run}"
+mvn clean -Djava.version.to.run="${java.version.to.run}"
 
 
-mvn -f for-release-pom.xml clean deploy -P sign-release,ossrh --settings ../../Installed/settings.xml --global-toolchains .github/ubuntu-toolchains.xml -DskipRemoteStaging=true -Djava.version.to.run="${java.version.to.run}"
+mvn -f for-release-pom.xml clean deploy -P sign-release,ossrh --settings ../../Installed/settings.xml -DskipRemoteStaging=true -Djava.version.to.run="${java.version.to.run}"
 
-mvn -f for-release-pom.xml nexus-staging:deploy-staged -P ossrh --settings ../../Installed/settings.xml --global-toolchains .github/ubuntu-toolchains.xml -DstagingDescription="Description of the staged repository" -Djava.version.to.run="${java.version.to.run}"
+mvn -f for-release-pom.xml nexus-staging:deploy-staged -P ossrh --settings ../../Installed/settings.xml -DstagingDescription="Description of the staged repository" -Djava.version.to.run="${java.version.to.run}"
 
 git clean -f
 git push
